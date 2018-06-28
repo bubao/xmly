24523860dd79092b42851c9cb056ad2d7ffe9d95
bubao <asd565586630@gmail.com>
2018-06-28 18:02:39 +0800

    const

--- src/index.js
+++ src/index.js
@@ -3,22 +3,20 @@
  * @description 
  * @date: 2018-06-27 18:11:17 
  * @Last Modified by: bubao
- * @Last Modified time: 2018-06-27 18:14:27
+ * @Last Modified time: 2018-06-28 11:58:13
  */
-let template = require("lodash/template");
-let concat = require("lodash/concat");
-let trim = require("lodash/trim");
-let cfg = require("./config/index");
-let { path, request } = require('./tools/commonModules');
-let fs = require("fs");
+const template = require("lodash/template");
+const concat = require("lodash/concat");
+const trim = require("lodash/trim");
+const cfg = require("./config/index");
+const { path, request, fs } = require('./tools/commonModules');
 
-
-let getTracksList = async (albumId, arr = [], pageNum = 1) => {
-	let opts = {
+const getTracksList = async (albumId, arr = [], pageNum = 1) => {
+	const opts = {
 		uri: template(cfg.getTracksList)({ albumId, pageNum })
 	}
 
-	let body = JSON.parse((await request(opts)).body);
+	const body = JSON.parse((await request(opts)).body);
 	arr = concat(arr, body.data.tracks);
 	if (!(body.data.tracks.length < 30) || pageNum * 30 === body.data.trackTotalCount) {
 		return await getTracksList(albumId, arr, pageNum + 1);
@@ -27,22 +25,20 @@ let getTracksList = async (albumId, arr = [], pageNum = 1) => {
 	}
 }
 
-const donwTracks = async (list, out = "./", str = "") => {
-	let item = list.splice(0, 1)[0];
-	let data = JSON.parse((await request({
+const getTracks = async (list, out = "./", str = "") => {
+	const item = list.splice(0, 1)[0];
+	const data = JSON.parse((await request({
 		uri: template(cfg.tracks)({ tracksID: path.basename(item.url) }),
 	})).body);
 	str += `${data.play_path_64}\n\tout=${trim(item.title)}.m4a\n\tdir=${out}\n`
 	if (list.length) {
-		return await donwTracks(list, out, str);
+		return await getTracks(list, out, str);
 	} else {
 		return str;
 	}
 }
 
-let xmly = async (albumId, path = "./list.txt", out) => {
-	let list = await getTracksList(albumId);
-	fs.writeFileSync(path, await donwTracks(list, out));
+module.exports = async (albumId, path = "./list.txt", out) => {
+	const list = await getTracksList(albumId);
+	fs.writeFileSync(path, await getTracks(list, out));
 }
-
-module.exports = xmly;
