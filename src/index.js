efb17c20856505962c08633c21a1d57bd22ffb4c
bubao <asd565586630@gmail.com>
2018-09-14 23:27:44 +0800

    add Down

--- src/index.js
+++ src/index.js
@@ -3,14 +3,28 @@
  * @description 
  * @date: 2018-09-14 19:08:25 
  * @Last Modified by: bubao
- * @Last Modified time: 2018-09-14 19:10:32
+ * @Last Modified time: 2018-09-14 23:10:46
  */
 let tracks = require("./modules/tracks");
 let albums = require("./modules/albums");
 let aria2c = require("./tools/aria2c");
 
+let Down = async (ID, out) => {
+	let al = await albums(ID)
+	let trackArray = []
+	al.forEach(element => {
+		trackArray.push(await tracks(element))
+	});
+	let str = ""
+	trackArray.forEach(element => {
+		str += aria2c(element, out);
+	});
+	return str;
+}
+
 module.exports = {
 	tracks,
 	albums,
-	aria2c
+	aria2c,
+	Down,
 }
