8f5be9fcb4f9dc15503cfbc1f3f2aea5bb4b34a7
bubao <asd565586630@gmail.com>
2018-11-10 19:14:18 +0800

    filenamify

--- src/tools/aria2c.js
+++ src/tools/aria2c.js
@@ -3,20 +3,21 @@
  * @description 
  * @date: 2018-09-14 19:08:17 
  * @Last Modified by: bubao
- * @Last Modified time: 2018-09-15 23:12:47
+ * @Last Modified time: 2018-11-10 19:09:19
  */
 
+const filenamify = require("filenamify");
 let aria = (list = [], dir) => {
-    let str = "";
-    if (list.length) {
-        list.forEach((value) => {
-            let { uri, title, albumTitle } = value
-            str += `${uri}\n\tout=${title.trim()}.m4a\n\tdir=${dir ? dir : "./"}${albumTitle.trim()}\n`
-        });
-        return str;
-    } else {
-        return str;
-    }
+	let str = "";
+	if (list.length) {
+		list.forEach((value) => {
+			let { uri, title, albumTitle } = value
+			str += `${uri}\n\tout=${filenamify(title.trim())}.m4a\n\tdir=${dir ? dir : "./"}${filenamify(albumTitle.trim())}\n`
+		});
+		return str;
+	} else {
+		return str;
+	}
 }
 
 module.exports = aria;
